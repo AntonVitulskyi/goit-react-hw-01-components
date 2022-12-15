df241803f9829d4b6f5f7bc8589f3422cf71f2fe
AntonVitulskyi <vitulskiyanton@gmail.com>
2022-12-15 23:47:22 +0200

    Update TransactionHistoryItem.jsx

--- src/components/TransactionHistory/TransactionHistoryItem.jsx
+++ src/components/TransactionHistory/TransactionHistoryItem.jsx
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 
-import styles from './TransactionHistory.module.css'
-
 export default function TransactionHistoryItem ({ type, amount, currency}) {
 	return (
         <tr>
